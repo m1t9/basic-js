a34cdfd87cfc90bf0e2e04180d3289be5c29ddd1
m1t9 <dpleshanov@gmail.com>
2020-09-10 16:04:09 +0300

    add: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+	let dreamTeam = '';
+	if (!Array.isArray(arguments[0])) return false;
+	arguments[0].forEach(item => {
+	(typeof item === 'string' && !/[0-9]/.test(item) && item.length !== 0) ?
+  		dreamTeam += item.replace(/\s/g, '').toUpperCase()[0] : false;
+	});
+	return dreamTeam.split('').sort().join('');
 };
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 require('./test/count-cats.test');
 require('./test/carbon-dating.test');
-// require('./test/dream-team.test');
+require('./test/dream-team.test');
 // require('./test/extended-repeater.test');
 // require('./test/what-season.test');
 // require('./test/vigenere-cipher.test');

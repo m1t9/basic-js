d2c5f587e944b5549ee9f27022193e8ea5cee32b
m1t9 <dpleshanov@gmail.com>
2020-09-11 01:57:53 +0300

    add: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+	if (!Array.isArray(arr)) throw new Error();
+
+	let result = arr.reduce((total, next, index) => {
+		
+  		if (next === '--discard-next' ||
+  			next === '--discard-prev' ||
+  			next === '--double-prev' ||
+  			next === '--double-next'
+  			) return total;
+
+		if (arr[index - 1] === '--discard-next') return total;
+
+		(arr[index - 1] === '--double-next') ? total.push(next, next) : total.push(next);
+
+		if (arr[index + 1] === '--double-prev') total.push(next);
+
+		if(arr[index + 1] === '--discard-prev') total.splice(total.length - 1, 1);
+
+		return total;
+
+	}, []);
+
+	return result;
 };

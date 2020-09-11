87aa85a6d3dfa8ecf60075aa0d59243a078fa0c0
m1t9 <dpleshanov@gmail.com>
2020-09-11 22:23:14 +0300

    add: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+	var repeat;
+	var separator;
+	
+	var additionStr = '';
+	if (options['addition'] !== undefined) {
+		repeat = (options['additionRepeatTimes'] !== undefined) ? options['additionRepeatTimes'] : 1;
+		separator = (options['additionSeparator'] !== undefined) ? options['additionSeparator'] : '|';
+		for (let repeatLocal = 1; repeatLocal <= repeat; repeatLocal++)
+			(repeatLocal !== repeat) ? (additionStr += options['addition'] + separator) : (additionStr += options['addition']);
+	}
+
+	var mainStr = '';
+	repeat = (options['repeatTimes'] !== undefined) ? options['repeatTimes'] : 1;
+	separator = (options['separator'] !== undefined) ? options['separator'] : '+';
+	for (let repeatLocal = 1; repeatLocal <= repeat; repeatLocal++)
+		(repeatLocal !== repeat) ? (mainStr += str + additionStr + separator) : (mainStr += str + additionStr);
+	
+	return mainStr;
 };
-  

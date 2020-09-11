b1973c51a295045269255ff632dd0a3357a49290
m1t9 <dpleshanov@gmail.com>
2020-09-11 03:32:13 +0300

    add: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chainRing : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainRing.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainRing.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ((position - 1 >= 0 && position - 1 <= this.chainRing.length - 1)
+      && position !== '' && this.chainRing[position] !== undefined && !isNaN(position)
+      ) {
+      this.chainRing.splice(position - 1, 1)
+    } else {
+      this.chainRing = [];
+      throw new Error();
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainRing.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+
+    this.chainRing.slice(0, this.chainRing.length - 1).map((item, index) => {
+      result += '( ' + item + ' )~~'
+    });
+    result += '( ' + this.chainRing[this.chainRing.length - 1] + ' )';
+
+    this.chainRing = [];
+    return result;
   }
 };
 
